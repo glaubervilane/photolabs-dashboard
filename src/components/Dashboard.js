dc2fedbfdd72c2459473305934260df5cf58d12a
Glauber Marques <glauberfmarques@gmail.com>
2023-08-29 21:44:02 -0600

    Initiate state management indashbaord extending to its components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,44 +1,66 @@
 import React, { Component } from "react";
 import Loading from "./Loading";
-
+import Panel from "./Panel";
 import classnames from "classnames";
 
 const data = [
   {
     id: 1,
     label: "Total Photos",
-    value: 10
+    value: 10,
   },
   {
     id: 2,
     label: "Total Topics",
-    value: 4
+    value: 4,
   },
   {
     id: 3,
     label: "User with the most uploads",
-    value: "Allison Saeng"
+    value: "Allison Saeng",
   },
   {
-    id: 3,
+    id: 4,
     label: "User with the most uploads",
-    value: "Lucas Souza"
+    value: "Lucas Souza",
   },
 ];
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null,
+  };
+
+  selectPanel = (id) => {
+    this.setState({
+      focused: id,
+    });
   };
 
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused,
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    return <main className={dashboardClasses} />;
+    const panels = (this.state.focused
+      ? data.filter((panel) => this.state.focused === panel.id)
+      : data
+    ).map((panel) => (
+      <Panel
+        key={panel.id}
+        id={panel.id}
+        label={panel.label}
+        value={panel.value}
+        onSelect={this.selectPanel}
+      />
+    ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
